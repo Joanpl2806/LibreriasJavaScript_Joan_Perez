83ea889ccb35aaa62f9568bccde5d49049efabb0
joanperez <joanpl2806@gmail.com@mail.com>
2019-10-02 15:10:17 -0400

    se agregó funciones para llenar la tabla de dulces

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(function(){
   $('.btn-reinicio').click(function(){
     cambiarColor1($('.main-titulo'))
+    llenarTabla()
   })
 });
 
@@ -19,3 +20,20 @@ function cambiarColor2(elemento){
     cambiarColor1(elemento)
   })
 };
+
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function generarDulce(numCol){
+  var n = numeroRandom(1,5)
+  $('.col-'+numCol).prepend($('<img src="image/'+n+'.png">'))
+}
+
+function llenarTabla(){
+  for(let i=1; i<8; i++){
+    for(let j=1; j<7; j++){
+      generarDulce(i)
+    }
+  }
+}
