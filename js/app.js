ad9b2cb581dc9bff7e3fae019ca5a8e860d39ffb
joanperez <joanpl2806@gmail.com@mail.com>
2019-10-03 15:13:47 -0400

    Se agregaron funciones para validar los movimientos de los dulces, draggable y droppable

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
+var leftIni, leftFin, topIni, topFin
+
 $(function(){
   $('.btn-reinicio').click(function(){
     cambiarColor1($('.main-titulo'))
     llenarTabla()
+    $('img').draggable({
+      start: function(event,ui){
+        leftIni = $(ui.offset.left)[0]
+        topIni = $(ui.offset.top)[0]
+      }
+    })
+    $('img').droppable({
+      drop: function(event,ui){
+        leftFin = $(ui.draggable)[0].offsetLeft
+        topFin = $(ui.draggable)[0].offsetTop
+        if(validarMovimiento() == true){
+          $(ui.draggable).draggable({revert: 'valid'})
+          var img1 = $($(ui.draggable)[0]).attr('src')
+          var img2 = $(this).attr('src')
+          $($(ui.draggable)[0]).attr('src',img2)
+          $(this).attr('src',img1)
+        }else {
+          $(ui.draggable).draggable({revert: 'valid'})
+        }
+      }
+    })
   })
 });
 
@@ -35,5 +58,79 @@ function llenarTabla(){
     for(let j=1; j<7; j++){
       generarDulce(i)
     }
+  }
+}
+
+function validarMovimiento(){
+  if(movDerecha() == true){
+    return true
+  }else if (movIzquierda() == true) {
+    return true
+  }else if (movArriba() == true) {
+    return true
+  }else if (movAbajo() == true) {
+    return true
+  }else {
+    return false
+  }
+}
+
+function movDerecha(){
+  var left1 = leftIni+110
+  var left2 = leftIni+260
+  var top1 = topIni-50
+  var top2 = topIni+132
+  var leftF1 = leftFin+110
+  var topF1 = topFin+110
+
+  if((leftFin >= left1 && leftF1 <= left2) && (topFin >= top1 && topF1 <= top2)){
+    return true
+  }else{
+    return false
+  }
+}
+
+function movIzquierda(){
+  var left1 = leftIni-140
+  var left2 = leftIni+30
+  var top1 = topIni-50
+  var top2 = topIni+132
+  var leftF1 = leftFin+110
+  var topF1 = topFin+110
+
+  if((leftFin >= left1 && leftF1 <= left2) && (topFin >= top1 && topF1 <= top2)){
+    return true
+  }else{
+    return false
+  }
+}
+
+function movArriba(){
+  var left1 = leftIni-50
+  var left2 = leftIni+150
+  var top1 = topIni-150
+  var top2 = topIni+50
+  var leftF1 = leftFin+110
+  var topF1 = topFin+110
+
+  if((leftFin >= left1 && leftF1 <= left2) && (topFin >= top1 && topF1 <= top2)){
+    return true
+  }else{
+    return false
+  }
+}
+
+function movAbajo(){
+  var left1 = leftIni-50
+  var left2 = leftIni+150
+  var top1 = topIni+110
+  var top2 = topIni+260
+  var leftF1 = leftFin+110
+  var topF1 = topFin+110
+
+  if((leftFin >= left1 && leftF1 <= left2) && (topFin >= top1 && topF1 <= top2)){
+    return true
+  }else{
+    return false
   }
 }
